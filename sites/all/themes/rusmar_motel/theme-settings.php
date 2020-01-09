77dc54c793705cf3f59b3d99e80b2195f7b16885
Matthew B Cannon <matthew.cannon@ffwagency.com>
2020-01-08 20:19:50 -0500

    Updated theme with Rio settings.

---
+++ sites/all/themes/rusmar_motel/theme-settings.php
@@ -0,0 +1,16 @@
+<?php
+
+function rusmar_motel_form_system_theme_settings_alter(&$form, $form_state) {
+  dpm($form);
+  $form['is_rio'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Is Rio Motel site?.'),
+    '#default_value' => theme_get_setting('is_rio'),
+  );
+
+  $form['logo_rio_url'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Path to custom Rio logo'),
+    '#default_value' => theme_get_setting('logo_rio_url'),
+  );
+}
